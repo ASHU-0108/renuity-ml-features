78648ce2fba80857f5e5128df8eb3436ae43b40b
Ashutosh Kumar Singh <ashutoshsinghvns08@gmail.com>
2026-03-10 17:12:58 +0530

    Remove ziptimezone dependency to fix C++ build error

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="renuity-ml-features",
-    version="1.0.3",
+    version="1.0.4",  # bumped version
     description="Shared feature engineering library for Renuity ML",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=[
-        "ziptimezone",
-    ],
+    install_requires=[], # Empty now!
     python_requires=">=3.8",
 )
