af64f0ec1aa05d16a10469a42babb02355d455ab
Ashutosh Kumar Singh <ashutoshsinghvns08@gmail.com>
2026-03-10 16:59:15 +0530

    Definitely removed ziptimezone

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="renuity-ml-features",
-    version="1.0.0",
+    version="1.0.3",
     description="Shared feature engineering library for Renuity ML",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
